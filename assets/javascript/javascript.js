d48fc58b9208e0273c1d458a2f1d40a89c26cca0
RiCK BROWN <ricky.brown.00@gmail.com>
2018-07-11 22:41:12 +0100

    New branch

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
-    let i = 1;
-    setInterval(function(){
-        let fact = facts[i];
-        document.getElementById('quickFact').innerHTML = fact;
-        i++
-        if(i >= facts.length){
-            i = 0;
-        }
-    }, 5000);
-}); // End of DOCUMENT.READY
+	let i = 1
+	setInterval(function(){
+		let fact = facts[i]
+		document.getElementById('quickFact').innerHTML = fact
+		i++
+		if(i >= facts.length){
+			i = 0
+		}
+	}, 5000)
+}) // End of DOCUMENT.READY
